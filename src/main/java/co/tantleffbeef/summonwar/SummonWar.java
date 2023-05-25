9ef4eb079182db7399da4fd8a46825b91f1db8c2
Xander <starmoose12@gmail.com>
2023-05-24 20:24:09 -0400

    Hello World!

---
+++ src/main/java/co/tantleffbeef/summonwar/SummonWar.java
@@ -0,0 +1,17 @@
+package co.tantleffbeef.summonwar;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SummonWar extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
