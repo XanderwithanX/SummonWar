1262234c38d72281cfb3da6e67c5257e1cb0d610
Xander <starmoose12@gmail.com>
2023-08-24 00:28:32 -0400

    Fixing StartGameGUI

--- src/main/java/co/tantleffbeef/summonwar/StartGameGUI.java
+++ src/main/java/co/tantleffbeef/summonwar/StartGameGUI.java
@@ -17,31 +17,31 @@ import org.bukkit.plugin.Plugin;
 public class StartGameGUI implements Listener {
     @EventHandler
     public void onClick(InventoryClickEvent event) {
-        Player player = (Player) event.getWhoClicked();
-        if(event.getInventory().getSize() == 9 && event.getInventory().getLocation() == null) {
-            event.setCancelled(true);
-            int temp = event.getSlot();
-
-            if(temp == 2) {
-                // tell thing that player is on team Crimson
-                event.getInventory().getItem(2).addEnchantment(Enchantment.DURABILITY, 1);
-            } else if (temp == 4) {
-                // tell thing that player is not on team
-                player.closeInventory();
-            } else if (temp == 6) {
-                // tell thing that player is on team Warped
-                event.getInventory().getItem(2).addEnchantment(Enchantment.DURABILITY, 1);
-
-            }
-        }
+//        Player player = (Player) event.getWhoClicked();
+//        if(event.getInventory().getSize() == 9 && event.getInventory().getLocation() == null) {
+//            event.setCancelled(true);
+//            int temp = event.getSlot();
+//
+//            if(temp == 2) {
+//                // tell thing that player is on team Crimson
+//                event.getInventory().getItem(2).addEnchantment(Enchantment.DURABILITY, 1);
+//            } else if (temp == 4) {
+//                // tell thing that player is not on team
+//                player.closeInventory();
+//            } else if (temp == 6) {
+//                // tell thing that player is on team Warped
+//                event.getInventory().getItem(2).addEnchantment(Enchantment.DURABILITY, 1);
+//
+//            }
+//        }
     }
 
     @EventHandler
     public void OnClose(InventoryCloseEvent event) {
-        Player player = (Player) event.getPlayer();
-        if(event.getInventory().getType() == InventoryType.HOPPER && event.getInventory().getLocation() == null) {
-
-        }
+//        Player player = (Player) event.getPlayer();
+//        if(event.getInventory().getType() == InventoryType.HOPPER && event.getInventory().getLocation() == null) {
+//
+//        }
     }
 
     //    private ItemStack makeItem(Material mat, String name, String... lore) {
